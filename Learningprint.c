02cb62eefddec4aea3ec0ec43eda8bf57d2370b6
Ishar786 <isharsheik33863@gmail.com>
2024-11-30 23:19:02 +0530

    Update Learningprint.c

--- Learningprint.c
+++ Learningprint.c
@@ -1,1 +1,19 @@
+int main() {
+    printf("--------------------------------\n");
+    printf("ref no\t|\titem\t    |\tprice\n");
+    printf("--------------------------------\n");
+    printf("3678\t|\tbanana\t    |\t60\n");
+    printf("3694\t|\tpapaya\t    |\t70\n");
 
+    printf("3344\t|\tcurdpackets |   95\n");
+  
+    printf("3124\t|\teggs\t    |\t74\n");
+ 
+    printf("1288\t|\ttomatoes\t|\t30\n");
+
+    printf("Total \t\t\t\t\t    329");
+    // Write C code here
+    printf("\n\nSheik Ishar");
+
+    return 0;
+}
